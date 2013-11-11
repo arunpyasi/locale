f70a418a197befd328dcccdad84d00acbf96febd
Arun Kumar Pyasi <expertaruncorp@gmail.com>
2013-11-11 08:59:30 -0800

    Create imp.c

---
+++ imp.c
@@ -0,0 +1,6 @@
+Links which are needed :-
+https://wiki.ubuntu.com/Translations/KnowledgeBase/StartingTeam
+https://wiki.ubuntu.com/Translations/KnowledgeBase/AddingNewLanguage#POSIX
+https://help.ubuntu.com/community/Locale
+https://launchpad.net/~ubuntu-langpack/+archive/ppa
+http://askubuntu.com/questions/95190/what-is-an-ubuntu-localized-image-and-how-do-i-create-one
